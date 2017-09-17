31a41863ab9effd6be2ccded99a75b58ac2533b6
hikaruna <me@hikaruna.net>
2017-09-18 00:51:59 +0900

    add javadoc

--- src/main/java/net/hikaruna/rolling/VoidFunction.java
+++ src/main/java/net/hikaruna/rolling/VoidFunction.java
@@ -1,6 +1,17 @@
 package net.hikaruna.rolling;
 
+/**
+ * 返り値がvoidのFunction.
+ *
+ * @param <Arg> 引数の型
+ */
 @SuppressWarnings("WeakerAccess")
 public interface VoidFunction<Arg> {
+
+    /**
+     * Functionを評価する.
+     *
+     * @param arg 引数
+     */
     void call(Arg arg);
 }
