8444bb622a7a84fedc6f5fce05eece5092dd3f99
hikaruna <me@hikaruna.net>
2017-09-20 00:18:22 +0900

    add Collection, List interface

---
+++ src/main/java/net/hikaruna/rolling/List.java
@@ -0,0 +1,10 @@
+package net.hikaruna.rolling;
+
+/**
+ * {@link java.util.List}に、map-reduce等の機能を追加した拡張版.
+ *
+ * @param <E> the type of elements in this list
+ */
+@SuppressWarnings("WeakerAccess")
+public interface List<E> extends java.util.List<E>, Collection<E> {
+}
