45532ca5bf463df8a00e8b5eba3ff9f254185ba8
hikaruna <me@hikaruna.net>
2017-09-20 01:08:56 +0900

    add enumerable

--- src/main/java/net/hikaruna/rolling/List.java
+++ src/main/java/net/hikaruna/rolling/List.java
@@ -1,5 +1,7 @@
 package net.hikaruna.rolling;
 
+import javax.annotation.Nonnull;
+
 /**
  * {@link java.util.List}に、map-reduce等の機能を追加した拡張版.
  *
@@ -7,4 +9,7 @@ package net.hikaruna.rolling;
  */
 @SuppressWarnings("WeakerAccess")
 public interface List<E> extends java.util.List<E>, Collection<E> {
+
+    @Override
+    <Ret> List<Ret> map(@Nonnull final Function<Ret, E> function);
 }
